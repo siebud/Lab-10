39230411bbd5215d702e4742465494864447f674
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-04-12 12:49:46 -0500

    second iteration Triangle classes

--- IsoscelesTriangle.java
+++ IsoscelesTriangle.java
@@ -1,4 +1,11 @@
+import java.awt.Color;
+import java.awt.Point;
 
-public class IsoscelesTriangle {
+public class IsoscelesTriangle extends Triangle{
+	public IsoscelesTriangle(Point pointLB, int base, int height, Color color, boolean fill) {
+		super(pointLB, color, fill);
+		location[1] = new Point(pointLB.x, pointLB.y + height);
+		location[2] = new Point(pointLB.x + base, pointLB.y);
+	}
 
 }
--- RightTriangle.java
+++ RightTriangle.java
@@ -1,4 +1,11 @@
+import java.awt.Color;
+import java.awt.Point;
 
-public class RightTriangle {
+public class RightTriangle extends Triangle {
+	public RightTriangle (Point pointRA, int base, int height, Color color, boolean fill) {
+		super(pointRA, color, fill);
+		location[1] = new Point(pointRA.x + base, pointRA.y);
+		location[2] = new Point(pointRA.x, pointRA.y + height);
+	}
 
 }
