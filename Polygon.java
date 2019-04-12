896311be52a10a6b5d1ab80c32f34ef56296e466
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-04-12 12:21:33 -0500

    Polygon

--- Polygon.java
+++ Polygon.java
@@ -1,4 +1,11 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Polygon {
-
+public abstract class Polygon extends Shape{
+	public Polygon(Color color, boolean filled) {
+		super(color, filled);
+	}
+	public void draw(Graphics graphics) {
+		
+	}
 }
