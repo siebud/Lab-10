ee553dcd0481d164c5ebcd9988168eed58bcdb1a
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-04-16 21:01:26 -0500

    Square and rectangle

---
+++ Square.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class Square extends Rectangle {
+	public Square(Point pointUL, int width, Color color, boolean filled) {
+		super(pointUL, width, width, color, filled);
+	}
+
+}
